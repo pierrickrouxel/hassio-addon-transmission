4339810dba12c0fd9c2f31dd0018499d920ab4a2
Regev Brody <regevbr@gmail.com>
2021-09-23 23:49:52 +0300

    fix openvpn bug (#19)

--- transmission/rootfs/etc/cont-init.d/10-requirements.sh
+++ transmission/rootfs/etc/cont-init.d/10-requirements.sh
@@ -6,25 +6,25 @@
 # Check authentication requirements, if enabled
 if bashio::config.true 'authentication_required'; then
     if ! bashio::config.has_value 'username'; then
-        bashio::die 'Transmission authentication is enabled, but no username was specified'
+        bashio::exit.nok 'Transmission authentication is enabled, but no username was specified'
     fi
 
     if ! bashio::config.has_value 'password'; then
-        bashio::die 'Transmission authentication is enabled, but no password was specified'
+        bashio::exit.nok 'Transmission authentication is enabled, but no password was specified'
     fi
 fi
 
 # Check OpenVPN requirements, if enabled
 if bashio::config.true 'openvpn_enabled'; then
     if ! bashio::config.has_value 'openvpn_username'; then
-        bashio::die 'OpenVPN is enabled, but no username was specified'
+        bashio::exit.nok 'OpenVPN is enabled, but no username was specified'
     fi
 
     if ! bashio::config.has_value 'openvpn_password'; then
-        bashio::die 'OpenVPN is enabled, but no password was specified'
+        bashio::exit.nok 'OpenVPN is enabled, but no password was specified'
     fi
 
-    if ! bashio::file_exists "/config/openvpn/$(bashio::config.get 'openvpn_config').ovpn"; then
-        bashio::die "The configured /config/openvpn/$(bashio::config.get 'openvpn_config').ovpn file is not found"
+    if ! bashio::fs.file_exists "/config/openvpn/$(bashio::config 'openvpn_config').ovpn"; then
+        bashio::exit.nok "The configured /config/openvpn/$(bashio::config 'openvpn_config').ovpn file is not found"
     fi
 fi
